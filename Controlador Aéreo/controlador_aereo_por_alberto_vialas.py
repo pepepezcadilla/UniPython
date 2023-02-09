22f7a9a7700bd4212c9dbcf7c00540cc54c19baa
Alberto Vialas <albertoskyv@gmail.com>
2023-02-09 21:41:47 +0100

    Correciones ortográficas y comentario

--- Controlador Aéreo/controlador_aereo_por_alberto_vialas.py
+++ Controlador Aéreo/controlador_aereo_por_alberto_vialas.py
@@ -1,22 +1,28 @@
+#Importamos la librería time para simular el tráfico de aviones (sólo estética)
 import time
 
+#Creamos la clase Cola, para almacenar los vuelos que llegan.
 class Cola:
-
+    #Creamos el método constructor
     def __init__(self):
         self.items=[]
 
+    #Creamos el método para añadir elementos a la cola
     def encolar(self, vuelo):
         self.items.append(vuelo)
 
+    #Creamos el método para elominar elementos de la cola, salta un error si ya está vacía
     def desencolar(self):
         try:
             return self.items.pop(0)
         except:
             raise ValueError("La cola está vacía")
     
+    #Creamos el método para vaciar la cola
     def vaciarcola(self):
         self.items=[]
     
+    #Creamos el método para comprobar si la cola está vacía o no
     def colavacia(self):
         if(self.items==[]):
             return True
@@ -24,7 +30,7 @@ class Cola:
             return False
 
 
-
+#Creamos la función para escribir el fichero con los vuelos que entran, también llamamos a la función para leer el fichero y guardarlo en una lista.
 def escribevuelos(vuelo, prioridad, lista):
     with open("vuelos_in.txt", "a") as fichero:
         fichero.write(vuelo + prioridad + "\n")
@@ -33,12 +39,14 @@ def escribevuelos(vuelo, prioridad, lista):
     return lista
 
 
+#Creamos la función que escribe los vuelos que van aterrizando
 def escribeaterr(vuelo):
-    with open("aterrizajes.txt", "a")as fichero:
+    with open("aterrizados.txt", "a")as fichero:
         fichero.write(vuelo+"\n")
     fichero.close
 
 
+#Creamos una lista con 10 vuelos
 def vuelosiniciales(lista):
     escribevuelos("MAD1234", "n", lista)
     escribevuelos("CON6666", "p", lista)
@@ -52,6 +60,7 @@ def vuelosiniciales(lista):
     escribevuelos("JAV8346", "n", lista)
 
 
+#Creamos una función para leer el fichero donde están almacenados los vuelos y los pasa a una lista
 def leefichero(nombrefichero):
     with open(nombrefichero, "r")as fichero:
         lineas=fichero.readlines()
@@ -62,6 +71,7 @@ def leefichero(nombrefichero):
         return lineas
 
 
+#Creamos la función que llena dos colas, diferenciando entre vuelos prioritarios y no prioritarios
 def llenacola(lista, cola1, cola2):
     cola1.vaciarcola()
     cola2.vaciarcola()
@@ -74,6 +84,7 @@ def llenacola(lista, cola1, cola2):
     return(cola1, cola2)
 
 
+#Creamos una función para asignar cada vuelo a una pista, y vamos eliminandolos de las colas
 def asignapista(prio, noprio):
     pista1=""
     pista2=""
@@ -104,7 +115,7 @@ def asignapista(prio, noprio):
 
         time.sleep(1)
 
-
+#Inicio del programa
 colaprioritarios = Cola()
 colanoprioritarios = Cola()
 listavuelos = []
