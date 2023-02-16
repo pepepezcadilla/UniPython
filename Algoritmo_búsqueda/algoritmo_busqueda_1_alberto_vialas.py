5553dd7e9bda9d8dfb5cce4b26d706bce15ae321
Alberto Vialas <albertoskyv@gmail.com>
2023-02-16 17:15:29 +0100

    acabado

--- Algoritmo_búsqueda/algoritmo_busqueda_1_alberto_vialas.py
+++ Algoritmo_búsqueda/algoritmo_busqueda_1_alberto_vialas.py
@@ -1,19 +1,20 @@
-#Recorremos el array deseado, y si encontramos el valor, devolvemos True, si no, False
+#Recorremos el array deseado, y si encontramos el valor, devolvemos True y la posición, si no, False
 def busqueda(vector, valor):
     for i in range(len(vector)):
         if(vector[i]==valor):
-            return True
+            return True, i
     return False
 
 #Creamos un array e iniciamos el programa
-nuevoarray=[1, 8, 5, 3, 0, 75]
+nuevoarray=[1, 8, 5, 3, 0, 75, 9, 2, 10, 800]
 exit=False
 while(exit != True):
     buscado=input("qué valor desea buscar? (exit para salir): ")
     if(buscado=="exit"):
         exit=True
     else:
-        if(busqueda(nuevoarray, int(buscado))):
-            print("El valor existe")
+        buscar, posicion=busqueda(nuevoarray, int(buscado))
+        if(buscar):
+            print("El valor existe, está en la posición "+str(posicion))
         else:
             print("el valor no existe")
