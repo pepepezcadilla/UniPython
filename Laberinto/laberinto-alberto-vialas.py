60a996a33971572944674ef054caa82087fd1466
Alberto Vialas <albertoskyv@gmail.com>
2023-02-21 13:40:19 +0100

    acabado

--- Laberinto/laberinto-alberto-vialas.py
+++ Laberinto/laberinto-alberto-vialas.py
@@ -9,15 +9,17 @@ def crealaberinto():
     # Crear la entrada y las dos salidas (intersecciones 0, num_intersecciones+1 y num_intersecciones+2)
     long_ent = random.randint(1, 10)
     entrada = {"id": 0, "conexiones": [{"id": 1, "longitud": long_ent, "direccion": "i"}]}
-    salida_1 = {"id": num_intersecciones+1, "conexiones": []}
-    salida_2 = {"id": num_intersecciones+2, "conexiones": []}
+    salida_1 = {"id": num_intersecciones, "conexiones": []}
+    salida_2 = {"id": num_intersecciones+1, "conexiones": []}
     primera = {"id": 1, "conexiones": [{"id": 0, "longitud": long_ent, "direccion": "i"}]}
     # Crear las intersecciones y conectarlas entre sí
-    intersecciones = [entrada, salida_1, salida_2, primera]
+    intersecciones = [entrada, primera]
     for i in range(2, num_intersecciones):
         interseccion = {"id": i, "conexiones": []}
         intersecciones.append(interseccion)
 
+    intersecciones.append(salida_1)
+    intersecciones.append(salida_2)
     for i in range(1, num_intersecciones+1):
         # Elegir aleatoriamente las intersecciones a las que se va a conectar
         diccionario_1 = intersecciones[i]
@@ -27,7 +29,7 @@ def crealaberinto():
         num_conexiones2 = len(diccionario_2["conexiones"])
         if(num_conexiones2<4 and num_conexiones1<4):
             longitud = random.randint(1, 10)
-            direccion = random.choice(["i", "iv", "iv"])
+            direccion = random.choice(["i", "iv"])
             intersecciones[i]["conexiones"].append({"id": diccionario_2["id"], "longitud": longitud, "direccion": direccion})
             intersecciones[segundo]["conexiones"].append({"id": diccionario_1["id"], "longitud": longitud, "direccion": direccion})
 
@@ -50,12 +52,8 @@ def recorrelaberinto(intersecciones):
     final = False
 
     # Mientras no lleguemos a una salida, seguimos avanzando aleatoriamente
-    while actual["id"] not in [intersecciones[1]["id"], intersecciones[2]["id"]] and iteraciones < MAX_ITERACIONES:
-        print(actual["id"], intersecciones[1]["id"], intersecciones[2]["id"])
-        print(len(intersecciones))
-        if actual["id"] in [intersecciones[1]["id"], intersecciones[2]["id"]]:
-            final = True
-            break
+    while actual["id"] not in [10, 11] and iteraciones < MAX_ITERACIONES:
+        
         # Elegimos una conexión aleatoria
         posibles_destinos = [(conexion["id"], conexion["longitud"], conexion["direccion"]) for conexion in actual["conexiones"] if (conexion["direccion"] == "iv" or conexion["id"] > actual["id"])]
         if len(posibles_destinos) == 0:
@@ -73,6 +71,9 @@ def recorrelaberinto(intersecciones):
             longitud_final += longitud
             actual = next((item for item in intersecciones if item["id"] == destino), None) 
             
+        if actual["id"] in [10, 11]:
+            final = True
+            break
 
         iteraciones += 1
 
@@ -80,9 +81,15 @@ def recorrelaberinto(intersecciones):
     if(final):
         print("Hemos llegado a la salida pasando por las intersecciones:")
         print(" -> ".join(str(i) for i in camino))
-        print("Hemos tardado: ", longitud_final)
+        print("Hemos tardado:", longitud_final)
     else:
         print("Número de intentos demasiado alto. Hemos perdido.")
 
-laberinto=crealaberinto()
-recorrelaberinto(laberinto)
+#Inicio del programa
+salir=False
+while(salir!=True):
+    laberinto=crealaberinto()
+    recorrelaberinto(laberinto)
+    salida=str(input("Desea continuar? s/n: "))
+    if(salida=="n"):
+        salir=True
