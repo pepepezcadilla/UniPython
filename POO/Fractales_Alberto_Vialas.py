89d58e119fc0db317e805d1a231233c3fb1405a8
Alberto Vialas <albertoskyv@gmail.com>
2023-04-27 17:49:54 +0200

    acabado

--- POO/Fractales_Alberto_Vialas.py
+++ POO/Fractales_Alberto_Vialas.py
@@ -1,12 +1,12 @@
 import turtle
 import tkinter as tk
 
+
 class Fractal:
-    def __init__(self,turtle, longitud, factor, angulo, caso_base):
-        self.turtle = turtle
-        self.longitud = longitud
-        self.factor = factor
-        self. angulo = angulo
+    def __init__(self, longitud, factor, angulo, caso_base):
+        self.longitud = int(longitud)
+        self.factor = int(factor)
+        self. angulo = int(angulo)
         self.caso_base = caso_base
         
     def dame_longitud(self):
@@ -22,52 +22,93 @@ class Fractal:
         return self.caso_base
 
 class Arbol(Fractal):
-    def __init__(self, turtle, longitud, factor, angulo, caso_base):
-        super().__init__(turtle, longitud, factor, angulo, caso_base)
+    def __init__(self, longitud, factor, angulo, caso_base):
+        super().__init__(longitud, factor, angulo, caso_base)
 
-    def Arbol(self):
-        if self.longitud >= 1:
-            self.turtle.forward(self.longitud)
-            longitud_nueva = self.longitud - self.factor
-            self.turtle.left(self.angulo)
-            Arbol(self.turtle, longitud_nueva, self.factor, self.angulo)
-            self.turtle.right(self.angulo * 2)
-            Arbol(self.turtle, longitud_nueva, self.factor, self.angulo)
-            self.turtle.left(self.angulo)
-            self.turtle.backward(self.longitud)
+    def arbol(self):
+        if int(self.longitud) >= 1:
+            print(self.longitud)
+            t.forward(self.longitud)
+            longitud_vieja = self.longitud
+            self.longitud = self.longitud - self.factor
+            t.left(self.angulo)
+            arbol_izquierdo = Arbol(self.longitud, self.factor, self.angulo, self.caso_base)
+            arbol_izquierdo.arbol()
+            t.right(self.angulo * 2)
+            arbol_derecho = Arbol(self.longitud, self.factor, self.angulo, self.caso_base)
+            arbol_derecho.arbol()
+            t.left(self.angulo)
+            t.backward(longitud_vieja)
 
-#ME HE QUEDADO AQUI <-----------------------------------------------------------------------------------
-def Curva_Kotch(t, iteraciones, longitud, factor, angulo):
-    if iteraciones == 0: 
-        t.forward(longitud)
-    else: 
-        iteraciones = iteraciones-1
-        longitud = longitud/factor
-        Curva_Kotch(t, iteraciones, longitud, factor, angulo)
-        t.left(angulo)
-        Curva_Kotch(t, iteraciones, longitud, factor, angulo)
-        t.right(angulo*2)
-        Curva_Kotch(t, iteraciones, longitud, factor, angulo)
-        t.left(angulo)
-        Curva_Kotch(t, iteraciones, longitud, factor, angulo)
+class Curva_Koch(Fractal):
+    def __init__(self, iteraciones, longitud, factor, angulo, caso_base):
+        super().__init__(longitud, factor, angulo, caso_base)
+        self.iteraciones = int(iteraciones)
+        
+    def dibujar(self):
+        self.curva_koch(self.iteraciones, self.longitud)
+
+    def curva_koch(self, n, l):
+        if n == 0:
+            t.forward(self.longitud)
+            return
+        for angle in [self.angulo, -2 * self.angulo, self.angulo, 0]:
+            self.curva_koch(n-1, l*self.factor)
+            t.left(angle)
 
 # Clase para pintar el árbol y el koch
-class Pinta(Arbol, Curva_Kotch):
-    def arbol(self, mostrar_puntero, color, longitud, ordenado, angulo):
-        # Lógica para pintar el árbol
-        pass
+class Pinta():
+    def __init__(self, hide, color, arbolito, frackoch):
+        self.hide = hide
+        self.color = color
+        self.arbolito = arbolito
+        self.frackoch = frackoch
+
+        if(hide=="no"):
+            t.hideturtle()
+        t.color(color)
     
-    def koch(self, mostrar_puntero, color, iteraciones, longitud, factor, angulo):
-        # Lógica para pintar el koch
-        pass
+    def dame_oculta(self):
+        return self.hide
+    
+    def dame_color(self):
+        return self.color
+    
+    def arbol(self):
+        if(self.arbolito != None):
+            self.arbolito.arbol()
+    
+    def koch(self):
+        if(self.frackoch != None):
+            self.frackoch.dibujar()
+
+#Función para crear un árbol y pintarlo
+def pintararbol(hide, color, longitud, factor, angulo):
+    caso_base = "arbol"
+    arbol = Arbol(longitud, factor, angulo, caso_base)
+    pintar = Pinta(hide, color, arbol, None)
+    pintar.arbol()
+
+#Función para crear una curva de koch y pintarlo
+def pintarkoch(hide, color, iteraciones, longitud, factor, angulo):
+    caso_base = "Koch"
+    koch = Curva_Koch(iteraciones, longitud, factor, angulo, caso_base)
+    pintar = Pinta(hide, color, None, koch)
+    pintar.koch()
+    t.right(120)
+
+def inicio():
+    t.up()
+    t.goto(-200, 0)
+    t.down()
 
 # Clase para el menú
 class MenuGrafico:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Menú Gráfico")
-        self.pinta = Pinta()
         self.create_menu()
+     
 
     def create_menu(self):
         # Botones del menú
@@ -75,6 +116,8 @@ class MenuGrafico:
         arbol_button.pack()
         koch_button = tk.Button(text="Koch", command=self.opciones_koch)
         koch_button.pack()
+        cero_button = tk.Button(text="Volver al inicio", command=lambda: inicio())
+        cero_button.pack()
         limpiar_button = tk.Button(text="Limpiar", command=self.limpiar)
         limpiar_button.pack()
         salir_button = tk.Button(text="Salir", command=self.salir)
@@ -117,7 +160,7 @@ class MenuGrafico:
         angulo_entry.pack()
 
         # Botón de pintar
-        pintar_button = tk.Button(opciones_arbol_window, text="Pintar", command=lambda: self.pinta.arbol(
+        pintar_button = tk.Button(opciones_arbol_window, text="Pintar", command=lambda: pintararbol(
             mostrar_puntero_entry.get(), color_entry.get(), longitud_entry.get(), ordenado_entry.get(),
             angulo_entry.get()))
         pintar_button.pack()
@@ -168,7 +211,7 @@ class MenuGrafico:
         angulo_entry.pack()
 
         # Botón de pintar
-        pintar_button = tk.Button(opciones_koch_window, text="Pintar", command=lambda: self.pinta.koch(
+        pintar_button = tk.Button(opciones_koch_window, text="Pintar", command=lambda: pintarkoch(
             mostrar_puntero_entry.get(), color_entry.get(), iteraciones_entry.get(), longitud_entry.get(),
             factor_entry.get(), angulo_entry.get()))
         pintar_button.pack()
@@ -178,11 +221,34 @@ class MenuGrafico:
         volver_button.pack()
 
     def limpiar(self):
-        # Lógica para limpiar el lienzo
-        pass
+        t.clear
 
     def salir(self):
-        self.window.destroy()
+        exit()
 
+t = turtle.Turtle()
 menu_grafico = MenuGrafico()
 menu_grafico.window.mainloop()
+
+
+
+
+
+def curva_koch(self):
+    print(self.longitud)
+    if self.iteraciones == 0: 
+        t.forward(self.longitud)
+    else: 
+        self.iteraciones = self.iteraciones-1
+        self.longitud = self.longitud/self.factor
+        self.curva_koch()
+        t.forward(self.longitud)
+        t.left(self.angulo)
+        self.curva_koch()
+        t.forward(self.longitud)
+        t.right(self.angulo*2)
+        self.curva_koch()
+        t.forward(self.longitud)
+        t.left(self.angulo)
+        self.curva_koch()
+        t.forward(self.longitud)
