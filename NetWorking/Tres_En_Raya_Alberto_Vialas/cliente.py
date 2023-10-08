b61e365fddce24a1ad2e17dd9f40999b9a6d1f31
alber <albertoskyv@gmail.com>
2023-10-08 18:18:22 +0200

    avance

--- NetWorking/Tres_En_Raya_Alberto_Vialas/cliente.py
+++ NetWorking/Tres_En_Raya_Alberto_Vialas/cliente.py
@@ -3,15 +3,16 @@ import tkinter as tk
 from tkinter import messagebox
 import threading
 import json
+import time
 
 # Función para solicitar el modo de juego al usuario
 def choose_game_mode():
     while True:
-        mode = input("¿Deseas jugar contra otro jugador (J) o contra la máquina (M)? ").strip().upper()
-        if mode == "J":
-            return "J"
-        elif mode == "M":
-            return "M"
+        mode = input("¿Deseas jugar contra otro jugador (J) o contra la máquina (M)? ").strip().lower()
+        if mode == "j":
+            return "j"
+        elif mode == "m":
+            return "m"
         else:
             print("Por favor, ingresa 'J' para jugar contra otro jugador o 'M' para jugar contra la máquina.")
 
@@ -44,7 +45,7 @@ client.send(message.encode())
 # Ventana de juego
 root = tk.Tk()
 root.title("Tres en Raya")
-print("aaa")
+
 # Variables globales
 symbol = ""
 current_player = ""
@@ -54,10 +55,23 @@ game_over = False
 # Variable para indicar si el juego ha comenzado
 game_started = False
 
+# Función para manejar el clic del usuario en un botón del tablero
+def handle_button_click(row, col):
+    if current_player == player_name and not game_over:
+        if buttons[row][col]["text"] == "":
+            buttons[row][col]["text"] = str(symbol)
+            # Llama a send_move para enviar el movimiento al servidor
+            send_move(row, col)
+        else:
+            print("La casilla ya está ocupada. Elige otra.")
+    else:
+        print("No es tu turno. Espera a tu turno para realizar un movimiento.")
+
 # Función para enviar un movimiento al servidor
 def send_move(row, col):
     if current_player == player_name and not game_over:
         move = f"Move:{row}{col}"
+        print(f"Enviado movimiento: {move}")
         client.send(move.encode())
 
 # Función para manejar los movimientos recibidos del servidor
@@ -91,33 +105,48 @@ def show_result(result):
 
 # Función para manejar el flujo del juego
 def game_flow():
-    global current_player, game_started
-    while True:
+    global current_player, game_started, game_over, clicked_button, symbol
+    clicked_button = tk.StringVar()
+    while not game_over:
+        print('again')
         try:
             message = client.recv(1024).decode()
+            print(message)
             if message.startswith("Symbol:"):
-                global symbol
                 symbol = message.split(":")[1]
                 if symbol == "X":
                     current_player = player_name
                 else:
                     current_player = "Jugador 2"
+            
+            elif message.startswith("Your Turn:"):
+                current_player = player_name
+                print("Es tu turno. Selecciona una casilla (fila y columna):")
+            
             elif message.startswith("Board:"):
                 board = message.split(":")[1]
-                for row in range(3):
-                    for col in range(3):
-                        buttons[row][col]["text"] = board[row * 3 + col]
+                # Actualiza el tablero con la información recibida
+                handle_move(board)
+            
             elif message.startswith("Result:"):
                 result = message.split(":")[1]
                 show_result(result)
-            elif message.startswith("Your Turn:"):
-                current_player = player_name
-        except:
+                game_over = True
+            
+            elif message.startswith('Cambio'):
+                current_player = "Jugador 2"
+        
+        except Exception as e:
+            print("Error:", e)
             break
 
 # Esperar hasta que el juego haya comenzado
-if not game_started:
-    game_started = client.recv(1024).decode()
+while True:
+    game_status = client.recv(1024).decode()
+    print(game_status)
+    if game_status == 'GameStart':
+        print("El juego ha comenzado.")
+        break
 
 # Ventana de juego principal
 game_window = tk.Toplevel(root)
@@ -127,7 +156,7 @@ for row in range(3):
     button_row = []
     for col in range(3):
         button = tk.Button(game_window, text="", font=("Helvetica", 20), width=5, height=2,
-                           command=lambda row=row, col=col: send_move(row, col))
+                           command=lambda row=row, col=col: handle_button_click(row, col))
         button.grid(row=row, column=col)
         button_row.append(button)
     buttons.append(button_row)
--- NetWorking/Tres_En_Raya_Alberto_Vialas/servidor.py
+++ NetWorking/Tres_En_Raya_Alberto_Vialas/servidor.py
@@ -3,6 +3,7 @@ import threading
 import sqlite3
 import random
 import json
+import time
 
 # Inicialización del servidor
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,7 +35,7 @@ player_count = 0
 
 # Función para manejar a un cliente
 def handle_client(client):
-    
+    global player_count, current_player, game_mode, name
     # Recibir el mensaje del cliente
     message = client.recv(1024).decode()
 
@@ -44,21 +45,18 @@ def handle_client(client):
     # Obtener el nombre del jugador y el modo de juego
     name = data_received["name"]
     game_mode = data_received["game_mode"]
-    print(name)
-    print(game_mode)
     player_names[client] = name
     clients[name] = client
 
     print(f"{name} se ha conectado desde {client.getpeername()}")
 
-    global player_count
+    current_player = ''
     player_count += 1
 
-    if game_mode == "M":
+    if game_mode == "m":
         # Si el modo de juego es "M", crea la máquina como un cliente adicional
         machine_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #machine_client.connect((host, port))
-        machine_name = "Máquina"
+        machine_name = "maquina"
         clients[machine_name] = machine_client
         player_names[machine_client] = machine_name
         machine_thread = threading.Thread(target=start_game, args=(machine_client, game_mode))
@@ -73,32 +71,31 @@ def handle_client(client):
         remove_client(client)
         return
 
-    while True:
-        try:
-            message = client.recv(1024).decode()
-            if message:
-                if message.startswith("Move:"):
-                    print(f"{name} ha realizado un movimiento: {message[5:]}")
-                    broadcast(message, client)
-        except:
-            remove_client(client)
-            break
 
-    remove_client(client)
 
 # Función para que la máquina realice un movimiento aleatorio
 def machine_move(board):
     empty_cells = [(r, c) for r in range(3) for c in range(3) if board[r * 3 + c] == ""]
     return random.choice(empty_cells)
-
+# Funcion para cambiar de jugador
+def changePlayer(current_player, player1, player2):
+    if current_player == player1:
+        if game_mode == 'm':
+            current_player = 'maquina'
+        else: 
+            current_player = player2
+    else: 
+        current_player = player1
+    broadcast('Cambio', 'Anonymous')
 # Función para transmitir mensajes a todos los clientes
 def broadcast(message, sender_client):
-    for name, client in clients.items():
-        if client != sender_client:
-            try:
-                client.send(message.encode())
-            except:
-                remove_client(client)
+    # Crear una copia del diccionario
+    clients_copy = clients.copy()
+
+    for name, client in clients_copy.items():
+        if name != sender_client and name != "maquina":
+            client.send(message.encode())
+            time.sleep(0.5)
 
 # Función para eliminar a un cliente de la lista
 def remove_client(client):
@@ -133,59 +130,67 @@ def update_ranking(player_name, result):
 
 # Función para iniciar el juego
 def start_game(client, game_mode):
-    broadcast(True, 'Anonymous')
+    broadcast('GameStart', 'Anonymous')
     players = list(clients.keys())
-    player1 = players[0]
-    player2 = players[1]
+    global player1, player2
+    player1 = players[1]
+    player2 = players[2]
 
-    # Envía el símbolo correspondiente a cada jugador
+    # Asigna el símbolo correspondiente a cada jugador
     symbol1 = "X"
     symbol2 = "O"
     client1 = clients[player1]
     client2 = clients[player2]
-    print(clients)
+    print(client1, client2)
     client1.send(f"Symbol:{symbol1}".encode())
-    client2.send(f"Symbol:{symbol2}".encode())
-
+    if(game_mode != 'm'):
+        client2.send(f"Symbol:{symbol2}".encode())
+    time.sleep(0.5)
     # Tablero de juego
     board = [""] * 9
-
+    current_player = player1
+    current_client = client1
+    print(players)
     while True:
-        current_player = player1
-        current_client = client1
+        if current_player == "maquina" and game_mode == "m":
+            row, col = machine_move(board)
+        else:
+            current_client.send(f"Your Turn:".encode())
+            move = current_client.recv(1024).decode()
+            print(f"{name} ha realizado un movimiento: {move[5:]}")
+            row, col = map(int, move[5:])
 
-        while True:
-            if current_player == "Máquina" and game_mode == "M":
-                row, col = machine_move(board)
-            else:
-                current_client.send(f"Your Turn:".encode())
-                move = current_client.recv(1024).decode()
-                row, col = map(int, move[5:])
+        index = row * 3 + col
 
-            index = row * 3 + col
+        #if board[index] == "":
+            #   board[index] = symbol1 if current_player == player1 else symbol2
+            #  broadcast(f"Board:{''.join(board)}", current_client)
+#
+#               if check_winner(board, symbol1):
+#                  update_ranking(player1, player1)
+#                 broadcast(f"Result:{player1}", current_client)
+#                return
+    #           elif check_winner(board, symbol2):
+    #              update_ranking(player2, player2)
+    #             broadcast(f"Result:{player2}", current_client)
+    #            return
+        #       elif "" not in board:
+        #          update_ranking(player1, "Draw")
+        #         update_ranking(player2, "Draw")
+        #        broadcast("Result:Draw", current_client)
+            #       return
 
-            if board[index] == "":
-                board[index] = symbol1 if current_player == player1 else symbol2
-                broadcast(f"Board:{''.join(board)}", current_client)
+        # Muestra el tablero después del movimiento
+        #broadcast(f"Board:{''.join(board)}", current_client)
 
-                if check_winner(board, symbol1):
-                    update_ranking(player1, player1)
-                    broadcast(f"Result:{player1}", current_client)
-                    return
-                elif check_winner(board, symbol2):
-                    update_ranking(player2, player2)
-                    broadcast(f"Result:{player2}", current_client)
-                    return
-                elif "" not in board:
-                    update_ranking(player1, "Draw")
-                    update_ranking(player2, "Draw")
-                    broadcast("Result:Draw", current_client)
-                    return
+        # Cambio de jugador
+        print(current_player)
+        current_player = changePlayer(current_player, player1, player2)
 
-            if current_player == "Máquina" and game_mode == "M":
-                break
+        if current_player == "maquina" and game_mode == "M":
+            break
 
-            current_player, current_client = player2, client2
+
 
 # Función para comprobar si hay un ganador
 def check_winner(board, symbol):
@@ -199,8 +204,6 @@ while True:
     client, address = server.accept()
     print(f"Conexión establecida desde {address[0]}:{address[1]}")
     clients[player_names.get(client, 'Anonymous')] = client
-    client.send("Nombre de jugador: ".encode())
-    print(client)
     # Inicia un hilo para manejar al cliente
     client_handler = threading.Thread(target=handle_client, args=(client,))
     client_handler.start()
